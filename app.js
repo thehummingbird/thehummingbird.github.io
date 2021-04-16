786434e5afaaadf9e388ca3e898887a221276682
thehummingbird <sharadmaheshwari19@gmail.com>
2021-04-16 16:19:11 +0200

    update 8 with modals

--- app.js
+++ app.js
@@ -1,7 +1,61 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 
+const modalCvOpen = document.querySelector('.cvcard');
+const modalCv = document.querySelector('.modalcv');
+const modalCvbuttonClose = document.querySelector('.closecv');
+
+const modalMrOpen = document.querySelector('.mrcard');
+const modalMr = document.querySelector('.modalmr');
+const modalMrbuttonClose = document.querySelector('.closemr');
+
+const modalStOpen = document.querySelector('.stcard');
+const modalSt = document.querySelector('.modalst');
+const modalStbuttonClose = document.querySelector('.closest');
+
+const modalPdOpen = document.querySelector('.pdcard');
+const modalPd = document.querySelector('.modalpd');
+const modalPdbuttonClose = document.querySelector('.closepd');
+
+
 hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('toggle')
     navLinks.classList.toggle('show')
 });
+
+
+modalCvOpen.addEventListener('click',() =>{
+    modalCv.classList.toggle('active')
+})
+
+modalCvbuttonClose.addEventListener('click', () =>{
+    modalCv.classList.toggle('active')
+})
+
+
+modalMrOpen.addEventListener('click',() =>{
+    modalMr.classList.toggle('active')
+})
+
+modalMrbuttonClose.addEventListener('click', () =>{
+    modalMr.classList.toggle('active')
+})
+
+
+modalStOpen.addEventListener('click',() =>{
+    modalSt.classList.toggle('active')
+})
+
+modalStbuttonClose.addEventListener('click', () =>{
+    modalSt.classList.toggle('active')
+})
+
+
+modalPdOpen.addEventListener('click',() =>{
+    modalPd.classList.toggle('active')
+})
+
+modalPdbuttonClose.addEventListener('click', () =>{
+    modalPd.classList.toggle('active')
+})
+
